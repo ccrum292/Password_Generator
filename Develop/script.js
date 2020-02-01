1669a1073f3394b1c5442d314f98bbad6a417f93
ccrum292 <ccrum292@gmail.com>
2020-02-01 16:54:12 -0500

    Finished JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-/* // Assignment Code
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -11,64 +11,89 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); */
+generateBtn.addEventListener("click", writePassword);
 
-var num = ["0123456789"];
-var special = ["!#$%&'()*+,-./:;<>=?@[]^_{}~"];
-var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+
+
+
+
+function generatePassword() {
+
+var num = ["0","1","2","3","4","5","6","7","8","9"];
+var special = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";",";","<",">","=","?","@","[","]","^","_","{","}","~"];
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var promptArray = [];
 var passwordArray = [];
+var passwordLength;
+var password1 = "";
 
-alert ("What follows is a series of prompts for password criteria.")
+  alert ("What follows is a series of prompts for password criteria.")
 
-function validation(){
-  for (var i = 0; i < promptArray.length; i++) {
-    alert("You have selected the following as Criteria..." + promptArray[i]);
-  }
-}
-
-
-if (confirm("would you like to include password length as criteria?")) {
-  promptArray.push("\nPassword Length"); 
-  var length = prompt("Please choose a number no less than 8 and no more that 128.");
-  validation();
-}
-
-if (confirm("would you like to include lowercase letters as criteria?")) {
-  passwordArray.push(lowercase);
-  promptArray.push("\nLowercase Letters");
-  validation();
-}
-
-if (confirm("would you like to include uppercase letters as criteria?")) {
-  passwordArray.push(uppercase);
-  promptArray.push("\nUpercase Letters"); 
-  validation();
-}
-
-if (confirm("would you like to include numbers as criteria?")) {
-  passwordArray.push(num);
-  promptArray.push("\nNumbers"); 
-  validation();
-}
-
-if (confirm("would you like to include special characters as criteria?")) {
-  passwordArray.push(special);
-  promptArray.push("\nSpecial Characters");
-  validation();
-}
-
-var password = "";
-var length;
-
-  for (var i = 0; i < length; ++i) {
-      password += passwordArray.charAt(Math.floor(Math.random() * passwordArray.length));
+  function getRightNum () {
+    passwordLength = parseInt(prompt("Please choose a number no less than 8 and no more that 128."));
+    
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Password length must be no less than 8 and no more that 128.")
+      return getRightNum();
+    }
+    
+    else if (isNaN(passwordLength)){
+      alert("Password must be a number.")
+      return getRightNum();
   }
   
-alert(password);
+    return passwordLength;
+  }
+
+ 
+
+  getRightNum(); 
+  
+  console.log(passwordLength);
+  console.log(typeof(passwordLength))
+
+if (confirm("Would you like to include lowercase letters as criteria?")) {
+  passwordArray.push(lowercase);
+  promptArray.push("\nLowercase Letters"); 
+  alert("You have selected the following as Criteria..." + promptArray);
 
 
+
+}
+
+if (confirm("Would you like to include uppercase letters as criteria?")) {
+  passwordArray.push(uppercase);
+  promptArray.push("\nUpercase Letters"); 
+  alert("You have selected the following as Criteria..." + promptArray);
+
+}
+
+if (confirm("Would you like to include numbers as criteria?")) {
+  passwordArray.push(num);
+  promptArray.push("\nNumbers"); 
+  alert("You have selected the following as Criteria..." + promptArray);
+
+}
+
+if (confirm("Would you like to include special characters as criteria?")) {
+  passwordArray.push(special);
+  promptArray.push("\nSpecial Characters");
+  alert("You have selected the following as Criteria..." + promptArray);
+
+}
+
+
+
+  for (var i = 0; i < passwordLength; ++i) {
+      var whichArray = (Math.floor(Math.random() * passwordArray.length)) 
+      password1 = password1 + passwordArray[whichArray][Math.floor(Math.random() * passwordArray[whichArray].length)];
+      console.log(password1)
+  }
+  return password1;
+  
+ 
+}
 
 
 
